497ef01ba1f9ed30b564ba96fc201534916bcd26
Bohdan Stasiv <badyaxa@gmail.com>
2022-05-29 13:52:52 +0300

    refactor code

--- folder-name_first-module/src/main/java/com/badyaxa/FirstModule.java
+++ folder-name_first-module/src/main/java/com/badyaxa/FirstModule.java
@@ -2,6 +2,6 @@ package com.badyaxa;
 
 public class FirstModule {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("FirstModule: Hello world!");
     }
 }
